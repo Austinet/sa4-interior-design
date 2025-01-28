78d93fb415da24321d5ea615b61bdbc642257197
Austinet <austinetsplendour@gmail.com>
2025-01-28 01:15:35 +0100

    ch: completes the introductory page, fixes the learn more link responsiveness

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 // Pill loading animation
-const pillFill = document.getElementById('pill-fill')
-const pillSteps = document.querySelectorAll('.pill-step')
+const introPage = document.getElementById("introductory-page");
+const pillSteps = document.querySelectorAll(".pill-step");
+const pillFill = document.getElementById("pill-fill");
 
+let counter = 1;
+
+function fillStep() {
+  if (counter < pillSteps.length) {
+    pillSteps[counter++].classList.add("active");
+    setTimeout(fillStep, 800);
+  } else {
+
+    introPage.style.display = 'none'
+  }
+
+  return;
+}
+
+setTimeout(fillStep, 800);
